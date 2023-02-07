1a083f6486fdc882ee042aeb3f988a2d937155cf
filimonovSB <fsb2603@mail.ru>
2023-02-07 16:48:11 +0700

    add files

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,46 @@
+import React, { FC, MouseEventHandler, PropsWithChildren, useState } from 'react'
+
+interface ButtonProps extends PropsWithChildren{
+    color?:string,
+    padding?:number,
+    bd?:string,
+    br?:number,
+    bg?:string,
+    w?:number,
+    h?:number,
+    fz?:number,
+    onClick?: MouseEventHandler,
+}
+
+const Button:FC<ButtonProps> = (
+    {children, color='white', padding=0, bd='1px solid black', br='0', bg='white', w=100,h=50, fz=15, onClick}) => {
+    const [isHover, setIsHover] = useState(false);
+
+    const handleMouseEnter = () => {
+      setIsHover(true);
+   };
+
+   const handleMouseLeave = () => {
+      setIsHover(false);
+   };
+  return (
+    <button
+    onClick={onClick}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    style={{
+        width:`${w}px`,
+        height:`${h}px`,
+        color:`${color}px`,
+        padding:`${padding}px`,
+        border:`${bd}`,
+        borderRadius:`${br}%`,
+        fontSize:`${fz}px`,
+        backgroundColor: isHover ? '#FF6746' : `${bg}`,
+    }}
+    >{children}</button>
+  )
+
+}
+
+export default Button
---
+++ src/components/products-buttons/ProductsButtons.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+
+import { product } from '../../App'
+import Button from '../UI/Button'
+ 
+
+
+interface ProductsButtons{
+    products:Array<product>,
+    handleByeProduct:(id:number)  => void
+}
+
+const ProductsButtons:FC<ProductsButtons> = ({products, handleByeProduct}) => {
+
+
+
+  return (
+   <ButtonsStyled className="">
+        {products.map(product=>(
+            <Button key={product.id}
+            color="black" padding={0}  br={50} bg="#FF9F46" w={70} h={70} fz={28}
+            onClick={()=>{handleByeProduct(product.id)}}>
+             {product.id}
+            </Button>
+        ))}
+   </ButtonsStyled>
+  )
+}
+
+export default ProductsButtons
+
+const ButtonsStyled = styled.div`
+    display:grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap:5px;
+`
